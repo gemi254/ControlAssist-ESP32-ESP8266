68bfc721a046b688c3ebdd3f11bcd2a4d75399ce
gemi254 <gemi254@192.168.56.1>
2023-10-26 13:37:17 +0300

    Start an AP if no connection

--- examples/ControlAssist-Simple/ControlAssist-Simple.ino
+++ examples/ControlAssist-Simple/ControlAssist-Simple.ino
@@ -8,16 +8,15 @@
   #define ADC_PIN A0
 #endif
 
-//Define application name
-#define APP_NAME "ControlAssist-Simple"
-#define INI_FILE "/ControlAssist-Simple.ini"
-
 #define LOGGER_LOG_LEVEL 5
 #include <controlAssist.h>  // Control assist class
 
-unsigned long pingMillis = millis();  // Ping 
-const char st_ssid[]=""; // Put connection info here
+// Put connection info here. 
+// On empty an AP will be started
+const char st_ssid[]=""; 
 const char st_pass[]="";
+unsigned long pingMillis = millis();  // Ping 
+
 char chBuff[128];
 static bool buttonState = false;
 
@@ -83,24 +82,33 @@ void setup() {
   Serial.flush();
   LOG_I("Starting..\n");  
   
-  //Connect WIFI
-  LOG_E("Connect WIFI.\n");
-  WiFi.mode(WIFI_STA);
-  WiFi.begin(st_ssid, st_pass);
-  uint32_t startAttemptTime = millis();
-  while (WiFi.status() != WL_CONNECTED && millis() - startAttemptTime < 15000)  {
-    Serial.print(".");
-    delay(500);
-    Serial.flush();
-  }  
-  Serial.println();
+   //Connect WIFI?
+  if(strlen(st_ssid)>0){
+    LOG_E("Connect Wifi to %s.\n", st_ssid);
+    WiFi.mode(WIFI_STA);
+    WiFi.begin(st_ssid, st_pass);
+    uint32_t startAttemptTime = millis();
+    while (WiFi.status() != WL_CONNECTED && millis() - startAttemptTime < 15000)  {
+      Serial.print(".");
+      delay(500);
+      Serial.flush();
+    }  
+    Serial.println();
+  } 
   
   //Check connection
   if(WiFi.status() == WL_CONNECTED ){
     LOG_I("Wifi AP SSID: %s connected, use 'http://%s' to connect\n", st_ssid, WiFi.localIP().toString().c_str()); 
   }else{
     LOG_E("Connect failed.\n");
-    return;
+    LOG_I("Starting AP.\n");
+    String mac = WiFi.macAddress();
+    mac.replace(":","");
+    String hostName = "ControlAssist_" + mac.substring(6);
+    WiFi.mode(WIFI_AP_STA);
+    WiFi.softAP(hostName.c_str(),"",1);
+    LOG_I("Wifi AP SSID: %s started, use 'http://%s' to connect\n", WiFi.softAPSSID().c_str(), WiFi.softAPIP().toString().c_str());      
+    if (MDNS.begin(hostName.c_str()))  LOG_I("AP MDNS responder Started\n");     
   }
 
   //Setup webserver
@@ -121,9 +129,6 @@ void setup() {
   ctrl.begin();
 
   pinMode(ADC_PIN, INPUT);
-#if defined(ESP32)
-  analogReadResolution(12);
-#endif  
 }
 
 void loop() {
