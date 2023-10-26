ad4e8306bd46025e1f7a67e4642284f80fff4c53
gemi254 <gemi254@192.168.56.1>
2023-10-26 13:36:55 +0300

    Updated v1.0.1a

--- examples/ControlAssist-Minimal/ConfigAssist-Minimal.ino
+++ examples/ControlAssist-Minimal/ConfigAssist-Minimal.ino
@@ -1,17 +1,23 @@
 #if defined(ESP32)
   #include <WebServer.h>
-  WebServer server(80);
+  WebServer server(80);  
+  #include <ESPmDNS.h>  
 #else
+  #include <ESP8266WiFi.h>
   #include <ESP8266WebServer.h>  
   ESP8266WebServer  server(80);
+  #include <ESP8266mDNS.h>
 #endif
 
 #define LOGGER_LOG_LEVEL 5
 #include <controlAssist.h>  // Control assist class
 
-unsigned long pingMillis = millis();  // Ping 
-const char st_ssid[]=""; // Put connection info here
+// Put connection info here. 
+// On empty an AP will be started
+const char st_ssid[]=""; 
 const char st_pass[]="";
+unsigned long pingMillis = millis();  // Ping 
+
 
 ControlAssist ctrl; //Control assist class
 
@@ -25,24 +31,33 @@ void setup() {
   Serial.print("\n\n\n\n");
   Serial.flush();
   LOG_I("Starting..\n");  
-
-  //Connect
-  WiFi.mode(WIFI_STA);
-  WiFi.begin(st_ssid, st_pass);
-  uint32_t startAttemptTime = millis();
-  while (WiFi.status() != WL_CONNECTED && millis() - startAttemptTime < 15000)  {
-    Serial.print(".");
-    delay(500);
-    Serial.flush();
-  }  
-  Serial.println();
+  //Connect WIFI?
+  if(strlen(st_ssid)>0){
+    LOG_E("Connect Wifi to %s.\n", st_ssid);
+    WiFi.mode(WIFI_STA);
+    WiFi.begin(st_ssid, st_pass);
+    uint32_t startAttemptTime = millis();
+    while (WiFi.status() != WL_CONNECTED && millis() - startAttemptTime < 15000)  {
+      Serial.print(".");
+      delay(500);
+      Serial.flush();
+    }  
+    Serial.println();
+  } 
   
   //Check connection
   if(WiFi.status() == WL_CONNECTED ){
     LOG_I("Wifi AP SSID: %s connected, use 'http://%s' to connect\n", st_ssid, WiFi.localIP().toString().c_str()); 
   }else{
     LOG_E("Connect failed.\n");
-    return;
+    LOG_I("Starting AP.\n");
+    String mac = WiFi.macAddress();
+    mac.replace(":","");
+    String hostName = "ControlAssist_" + mac.substring(6);
+    WiFi.mode(WIFI_AP_STA);
+    WiFi.softAP(hostName.c_str(),"",1);
+    LOG_I("Wifi AP SSID: %s started, use 'http://%s' to connect\n", WiFi.softAPSSID().c_str(), WiFi.softAPIP().toString().c_str());      
+    if (MDNS.begin(hostName.c_str()))  LOG_I("AP MDNS responder Started\n");     
   }
   
   //Setup webserver
