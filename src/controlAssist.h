5c4033562ddbc957e823e3e124fd39a04f52c293
gemi254 <gemi254@192.168.56.1>
2023-10-31 08:49:56 +0200

    Updated v1.0.3

--- src/controlAssist.h
+++ src/controlAssist.h
@@ -3,7 +3,7 @@
 
 #include <WebSocketsServer.h>
 
-#define CT_CLASS_VERSION "1.0.2"        // Class version
+#define CT_CLASS_VERSION "1.0.3"        // Class version
 
 // Define Platform libs
 #if defined(ESP32)
