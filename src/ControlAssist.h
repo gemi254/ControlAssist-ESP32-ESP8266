fae70369e2849be1268fdd129eb46bacfab875a9
gemi254 <gemi254@192.168.56.1>
2024-05-17 12:56:15 +0300

    Removed sstream

--- src/ControlAssist.h
+++ src/ControlAssist.h
@@ -1,11 +1,9 @@
 #if !defined(_CONTROL_ASSIST_H)
 #define  _CONTROL_ASSIST_H
 
-//#include <Arduino.h>
-#include <sstream>
 #include <vector>
 #if defined(ESP32)
-  #include <WebServer.h>  
+  #include <WebServer.h>
 #else
   #include <ESP8266WebServer.h>
   #include <LittleFS.h>
@@ -16,7 +14,7 @@
   #define LOGGER_LOG_LEVEL 4     // Set log level for this module
 #endif
 
-#define CT_CLASS_VERSION "1.1.0"        // Class version
+#define CT_CLASS_VERSION "1.1.1"        // Class version
 #define STREAM_CHUNKSIZE 256            // Stream file buffer size
 
 // Define Platform objects
@@ -26,7 +24,7 @@
   #else
     #define WEB_SERVER ESP8266WebServer
   #endif
-#endif 
+#endif
 
 #ifndef STORAGE
   #if defined(ESP32)
@@ -58,10 +56,10 @@ struct ctrlsNdx {
     size_t ndx;
 };
 
-class ControlAssist{ 
+class ControlAssist{
   public:
-    ControlAssist(); 
-    ControlAssist(uint16_t port);    
+    ControlAssist();
+    ControlAssist(uint16_t port);
     ~ControlAssist() {};
   public:
     // Start web sockets server
@@ -74,9 +72,9 @@ class ControlAssist{
     int  bind(const char* key);
     // Bind a html control with id = key to a control variable and an on event function
     int  bind(const char* key, WebSocketServerEvent ev);
-    // Bind a html control with id = key to a control variable with start int val 
-    int  bind(const char* key, const int val); 
-    // Bind a html control with id = key to a control variable with start char val 
+    // Bind a html control with id = key to a control variable with start int val
+    int  bind(const char* key, const int val);
+    // Bind a html control with id = key to a control variable with start char val
     int  bind(const char* key, const char* val);
     // Bind a html control with id = key to a control variable and an on event function
     int  bind(const char* key, const int val, WebSocketServerEvent ev);
@@ -87,25 +85,25 @@ class ControlAssist{
     void setGlobalCallback(WebSocketServerEventG ev);
     // Set html page code
     void setHtmlHeaders(const char *html_headers) { _html_headers = html_headers; }
-    void setHtmlBody(const char *html_body) { _html_body = html_body; }    
-    void setHtmlFooter(const char *html_footer) { _html_footer = html_footer; }    
+    void setHtmlBody(const char *html_body) { _html_body = html_body; }
+    void setHtmlFooter(const char *html_footer) { _html_footer = html_footer; }
     // Set html page code from file
     void setHtmlHeadersFile(const char *fileName) { _html_headers_file = fileName; }
-    void setHtmlBodyFile(const char *fileName) { _html_body_file = fileName; }    
-    void setHtmlFooterFile(const char *fileName) { _html_footer_file = fileName; }    
+    void setHtmlBodyFile(const char *fileName) { _html_body_file = fileName; }
+    void setHtmlFooterFile(const char *fileName) { _html_footer_file = fileName; }
     // Stop web sockets
     void close();
-  public:    
-    // Implement operator [] i.e. val = config['key']    
+  public:
+    // Implement operator [] i.e. val = config['key']
     String operator [] (String k) { return getVal(k); }
-    // Implement operator [] i.e. val = config[ndx]    
+    // Implement operator [] i.e. val = config[ndx]
     ctrlPairs operator [] (uint8_t ndx) { return _ctrls[ndx]; }
     // Return the val of a given key, Empty on not found
     String getVal(String key);
      // Return the position of given key
     int getKeyNdx(String key);
     // Return next key and value from configs on each call in key order
-    bool getNextPair(ctrlPairs &c);  
+    bool getNextPair(ctrlPairs &c);
     // Add vectors by key (name in ctrlPairs)
     size_t add(String key, String val);
     // Add vectors pairs
@@ -125,7 +123,7 @@ class ControlAssist{
     void sort();
     // Run websockets
     void loop() { if(_socket) _socket->loop(); }
-    // Get the initialization java script 
+    // Get the initialization java script
     String getInitScript();
     // Render html to client
     void sendHtml(WEB_SERVER &server);
