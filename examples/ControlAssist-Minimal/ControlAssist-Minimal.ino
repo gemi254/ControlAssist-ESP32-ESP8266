8ee07befe81e5e37a77de84da041e1a746293c6b
gemi254 <gemi254@192.168.56.1>
2024-05-18 08:52:42 +0300

    Updated v1.1.2

--- examples/ControlAssist-Minimal/ControlAssist-Minimal.ino
+++ examples/ControlAssist-Minimal/ControlAssist-Minimal.ino
@@ -1,8 +1,8 @@
 #include <ControlAssist.h>            // Control assist class
 
 #if defined(ESP32)
-  WebServer server(80);  
-  #include <ESPmDNS.h>  
+  WebServer server(80);
+  #include <ESPmDNS.h>
 #else
   ESP8266WebServer  server(80);
   #include <ESP8266mDNS.h>
@@ -19,24 +19,24 @@ void setup() {
   Serial.print("\n\n\n\n");
   Serial.flush();
   LOG_I("Starting..\n");
-    
+
   // Connect WIFI ?
   if(strlen(st_ssid)>0){
     LOG_E("Connect Wifi to %s.\n", st_ssid);
     WiFi.mode(WIFI_STA);
     WiFi.begin(st_ssid, st_pass);
     uint32_t startAttemptTime = millis();
-    while (WiFi.status() != WL_CONNECTED && millis() - startAttemptTime < 15000)  {
+    while (WiFi.status() != WL_CONNECTED && millis() - startAttemptTime < 20000)  {
       Serial.print(".");
       delay(500);
       Serial.flush();
-    }  
+    }
     Serial.println();
-  } 
-  
+  }
+
   // Check connection
   if(WiFi.status() == WL_CONNECTED ){
-    LOG_I("Wifi AP SSID: %s connected, use 'http://%s' to connect\n", st_ssid, WiFi.localIP().toString().c_str()); 
+    LOG_I("Wifi AP SSID: %s connected, use 'http://%s' to connect\n", st_ssid, WiFi.localIP().toString().c_str());
   }else{
     LOG_E("Connect failed.\n");
     LOG_I("Starting AP.\n");
@@ -45,21 +45,21 @@ void setup() {
     String hostName = "ControlAssist_" + mac.substring(6);
     WiFi.mode(WIFI_AP_STA);
     WiFi.softAP(hostName.c_str(),"",1);
-    LOG_I("Wifi AP SSID: %s started, use 'http://%s' to connect\n", WiFi.softAPSSID().c_str(), WiFi.softAPIP().toString().c_str());      
-    if (MDNS.begin(hostName.c_str()))  LOG_V("AP MDNS responder Started\n");     
+    LOG_I("Wifi AP SSID: %s started, use 'http://%s' to connect\n", WiFi.softAPSSID().c_str(), WiFi.softAPIP().toString().c_str());
+    if (MDNS.begin(hostName.c_str()))  LOG_V("AP MDNS responder Started\n");
   }
-  
+
   // Add a web server handler on url "/"
   ctrl.setup(server);
-  
+
   // Start web sockets
   ctrl.begin();
   LOG_I("ControlAssist started.\n");
-  
+
   // Start web server
   server.begin();
   LOG_I("HTTP server started\n");
-  
+
 }
 
 void loop() {
