96be994ef9719d90f7ed397719c03299091d29cc
gemi254 <gemi254@192.168.56.1>
2024-12-20 16:24:06 +0200

    Added websockets connections handler

--- examples/ControlAssist-BrowserLog/remoteLogViewer.h
+++ examples/ControlAssist-BrowserLog/remoteLogViewer.h
@@ -15,10 +15,41 @@ body{
   font-family: monospace;
   font-size: 0.7rem;
 }
+
+#conLed {
+  width: 10px;
+  height: 10px;
+  background-color: lightGray;
+  border-radius: 10px;
+  
+}
+#conLed.on {
+  background-color: lightGreen;
+  border: 1px solid #4CAF50;
+}
 </style>
 <body>
+<table><tr>
+  <td><div id="conLed">&nbsp;</td>
+  <td><span id="wsStatus">Disconnected</span></td>
+  </tr>
+</table>
+
 <input type="text" id="logLine" style="Display: None;">
 <span id="logText"></span>
+
+<script>
+  document.getElementById("wsStatus").addEventListener("change", (event) => {
+  conLed = document.getElementById("conLed")
+  if(event.target.innerHTML.startsWith("Connected: ")){
+    conLed.classList.add("on");
+  }else{
+    conLed.classList.remove("on");
+  }
+  conLed.title = event.target.innerHTML
+});
+</script>
+
 </body>
 )=====";
 
@@ -74,9 +105,20 @@ const toColor = (line) => {
 )=====";
 
 #include <ControlAssist.h>
-
-static class RemoteLogViewer *pLogView = NULL;  // Pointer to instance of log print viewer class
-#define MAX_LOG_BUFFER_LEN 8192                 // Maximum size of log buffer to store when disconnected
+// Log print arguments
+#define MAX_LOG_BUFFER_LEN 8192             // Maximum size of log buffer to store when disconnected
+#define MAX_LOG_FMT 256                     // Maximum size of log format
+static char fmtBuf[MAX_LOG_FMT];            // Format buffer
+static char outBuf[512];                    // Output buffer
+static va_list arglist;
+static File log_file;
+// Logger file open/close
+// Must called before LOG_I function to write to file and
+// closed on application shutdown
+#define LOGGER_OPEN_LOG()  if(!log_file) log_file = STORAGE.open(LOGGER_LOG_FILENAME, "a+")
+#define LOGGER_CLOSE_LOG() if(log_file) log_file.close()
+static class RemoteLogViewer *_pLogView = NULL;  // Pointer to instance of log print viewer class
+void wsStatusChange();                           // Handles connect / disconnect
 
 // Remote log print viewer class
 class RemoteLogViewer: public ControlAssist{
@@ -87,18 +129,76 @@ class RemoteLogViewer: public ControlAssist{
     RemoteLogViewer(uint16_t port) { ControlAssist::setPort(port);  _logBuffer = ""; }
     virtual ~RemoteLogViewer() { }
   public:
-    void setup(){
+    void init(){
       // Must not initalized
-      if(pLogView){
-        LOG_E("Already initialized with addr: %p\n", pLogView);
+      if(_pLogView){
+        LOG_E("Already initialized with addr: %p\n", _pLogView);
         return;
       }
       // Store this instance pointer to global
-      pLogView = this;
+      _pLogView = this;
       // Setup control assist
       ControlAssist::setHtmlBody(LOGGER_VIEWER_HTML_BODY);
       ControlAssist::setHtmlFooter(LOGGER_VIEWER_HTML_SCRIPT);
+      ControlAssist::bind("wsStatus","Disconnected", wsStatusChange);
       ControlAssist::bind("logLine");
+    }
+    // Delete the log file
+    void resetLogFile(){
+      if(!STORAGE.exists(LOGGER_LOG_FILENAME)) return;
+      LOG_W("Reseting log\n");
+      LOGGER_CLOSE_LOG();
+      STORAGE.remove(LOGGER_LOG_FILENAME);
+      // Reopen log file to store log lines
+      LOGGER_OPEN_LOG();
+    }
+
+    // Send server responce on /log
+    void handleLog(WEB_SERVER &server){
+      String res = "";
+      while( getHtmlChunk(res) ){
+        server.sendContent(res);
+      }
+      server.sendContent("");
+    }
+    // Reset or display log file
+    void handleLogFile(WEB_SERVER &server){
+      server.setContentLength(CONTENT_LENGTH_UNKNOWN);
+      if(server.hasArg("reset")){
+        resetLogFile();
+        server.sendContent("Reseted log");
+        return;
+      }
+      if(!STORAGE.exists(LOGGER_LOG_FILENAME)){
+        server.sendContent("Logging file not exists");
+        return;
+      }
+      LOGGER_CLOSE_LOG();
+      // Open for reading
+      File file = STORAGE.open(LOGGER_LOG_FILENAME, "r");
+      // Send log file contents
+      String res = "";
+      while( file.available() ){
+        res = file.readStringUntil('\n') + "\n";
+        server.sendContent(res);
+      }
+      file.close();
+      server.sendContent("");
+      // Reopen log file to store log lines
+      LOGGER_OPEN_LOG();
+    }
+    void sendBuffer(){
+      if(_logBuffer == "") return;
+      String logLine = "";
+      int l = 0;
+      // Send log buffer lines
+      while(_logBuffer.length() > 0){
+          l = _logBuffer.indexOf("\n", 0);
+          if(l < 0) break;
+          logLine = _logBuffer.substring(0, l );
+          ControlAssist::put("logLine", logLine );
+          _logBuffer = _logBuffer.substring(l + 1 , _logBuffer.length() );
+      }
     }
 
     // Custom log print function
@@ -106,16 +206,7 @@ class RemoteLogViewer: public ControlAssist{
       // Are clients connected?
       if(ControlAssist::getClientsNum()>0){
         if(_logBuffer != ""){
-            String logLine = "";
-            int l = 0;
-            // Send log buffer lines
-            while(_logBuffer.length() > 0){
-                l = _logBuffer.indexOf("\n", 0);
-                if(l < 0) break;
-                logLine = _logBuffer.substring(0, l );
-                ControlAssist::put("logLine", logLine );
-                _logBuffer = _logBuffer.substring(l + 1 , _logBuffer.length() );
-            }
+             sendBuffer();
             _logBuffer = "";
         }
         // Send current line
@@ -128,21 +219,14 @@ class RemoteLogViewer: public ControlAssist{
       if(_logBuffer.length() > MAX_LOG_BUFFER_LEN){
         int l = _logBuffer.indexOf("\n", 0);
         if(l >= 0)
-            _logBuffer = _logBuffer.substring(l + 1, _logBuffer.length() - 1);
+            _logBuffer = _logBuffer.substring(l + 1, _logBuffer.length());
       }
     }
   public:
     String _logBuffer;                      // String buffer to hold log lines
 };
 
-// Log print arguments
-#define MAX_LOG_FMT 256                     // Maximum size of log format
-static char fmtBuf[MAX_LOG_FMT];            // Format buffer
-static char outBuf[512];                    // Output buffer
-static va_list arglist;
-static File log_file;
-
-// Custom log print function.. Prints on pLogView instance
+// Custom log print function.. Prints on _pLogView instance
 void _log_printf(const char *format, ...){
   strncpy(fmtBuf, format, MAX_LOG_FMT);
   fmtBuf[MAX_LOG_FMT - 1] = 0;
@@ -150,11 +234,18 @@ void _log_printf(const char *format, ...){
   vsnprintf(outBuf, MAX_LOG_FMT, fmtBuf, arglist);
   va_end(arglist);
   Serial.print(outBuf);
-  if(pLogView) pLogView->print(outBuf);
+  if(_pLogView) _pLogView->print(outBuf);
   if(log_file){
     log_file.print(outBuf);
     log_file.flush();
   }
+}
+// Handle websocket connections
+void wsStatusChange(){
+  if(!_pLogView) return;
+  String wsStatus = _pLogView->getVal("wsStatus");
+  if(wsStatus.startsWith("Connected:"))
+    _pLogView->sendBuffer();
 }
 
 #endif
